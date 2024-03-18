7d5532a2c48e40c298fc2579e2c85327847da96d
adelina <141991621+delinwy@users.noreply.github.com>
2024-03-18 22:38:43 +0600

    first hw

---
+++ product/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    text = models.TextField()
+    product = models.ForeignKey(Product, related_name='reviews', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Review for {self.product.title}'
